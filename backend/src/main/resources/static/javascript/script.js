5925e484718ba235f6d5ca5efaaa0fc23477d0a7
Rhyanzin01 <dev.rhyan.alves@gmail.com>
2025-12-10 10:49:01 -0300

    Atualizção crud Java script frontend

--- backend/src/main/resources/static/javascript/script.js
+++ backend/src/main/resources/static/javascript/script.js
@@ -1,7 +1,71 @@
 function openTab(id){
-  document.querySelectorAll("section").forEach(s=>s.style.display="none");
-  document.getElementById(id).style.display="block";
+    document.querySelectorAll("section").forEach(s => s.style.display = "none");
+    document.getElementById(id).style.display = "block";
 }
+
+const API_URL = "http://localhost:8080/api/filmes";
+
+// Carrega lista ao abrir página
+document.addEventListener("DOMContentLoaded", listarFilmes);
+
+function listarFilmes() {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(filmes => {
+
+        const lista = document.getElementById("myMoviesGrid");
+        lista.innerHTML = "";
+
+        const params = new URLSearchParams(window.location.search);
+        const idGenero = params.get("idGenero");
+
+        if (idGenero) {
+            filmes = filmes.filter(f => f.genero && f.genero.id == idGenero);
+        }
+
+        if (filmes.length === 0) {
+            lista.innerHTML = `
+                <p style="grid-column: 1/-1; text-align: center; color:#666;">
+                    Nenhum filme encontrado.
+                </p>`;
+            return;
+        }
+
+        filmes.forEach(f => {
+            lista.innerHTML += `
+                <div class="card">
+                    <div class="acoes-filme">
+                        <button class="btn-editar" onclick="editarFilme(${f.id})">✏️</button>
+                        <button class="btn-excluir" onclick="excluirFilme(${f.id})">🗑️</button>
+                    </div>
+
+                    <img src="${f.urlCapa}" alt="${f.titulo}" 
+                         onerror="this.src='https://via.placeholder.com/300x450?text=Sem+Capa'">
+
+                    <div class="card-info">
+                        <strong>${f.titulo}</strong><br>
+                        <em>${f.genero ? f.genero.nome : "Sem Gênero"}</em><br>
+                        ${f.ano ? f.ano : ""} - ${f.diretor ?? ""}
+                    </div>
+                </div>`;
+        });
+    })
+    .catch(err => console.error("Erro ao carregar filmes", err));
+}
+
+function editarFilme(id){
+    window.location.href = `html/cadastroFilme.html?id=${id}`;
+}
+
+function excluirFilme(id){
+    fetch(`${API_URL}/${id}`, { method: "DELETE" })
+    .then(() => listarFilmes())
+    .catch(() => alert("Erro ao excluir"));
+}
+
+
+
+
 
 /* CARROSSEL AUTOMÁTICO */
 let index=0;
@@ -72,8 +136,9 @@ themeBtn.addEventListener("click",()=>{
   document.body.classList.toggle("dark");
 
   if(document.body.classList.contains("dark")){
-    themeBtn.innerText="Responsável";
+    themeBtn.innerText="Responsvel";
   }else{
     themeBtn.innerText="Kids";
   }
 });
+
